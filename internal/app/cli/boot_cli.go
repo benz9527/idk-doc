db47fba6a9a53da715ff8f0ae29bcafc2cd10255
Ben.Zheng <benzheng.6dev9527@outlook.com>
2022-08-09 12:15:54 +0800

    Added new constructor for boot command line.

--- internal/app/cli/boot_cli.go
+++ internal/app/cli/boot_cli.go
@@ -10,7 +10,11 @@ import (
 type BootOptions struct {
 	ListenAddr string `short:"A" long:"listen-addr" required:"true" default:"0.0.0.0" description:"Assign listening IP address for idk-doc application to boot up."`
 	ListenPort string `short:"p" long:"listen-port" required:"true" default:"9527" description:"Assign listening port number for idk-doc application to boot up."`
-	FilePath   string `short:"f" long:"cfg-file-path" required:"true" description:"Assign configuration file path for idk-doc application to boot up."`
+	FilePath   string `short:"f" long:"cfg-file-path" required:"true" default:"./conf/idk-boot.yaml" description:"Assign configuration file path for idk-doc application to boot up."`
+}
+
+func NewBootOptions() *BootOptions {
+	return &BootOptions{}
 }
 
 func (o *BootOptions) Parse(args []string) error {
