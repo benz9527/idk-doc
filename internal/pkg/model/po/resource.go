c5c314bd137e30272bfd39a4f15e9780a9656c45
Ben.Zheng <benzheng.6dev9527@outlook.com>
2022-08-18 15:09:59 +0800

    Added resource group, image and attachment po definitions then tests with empty sql file.

---
+++ internal/pkg/model/po/resource.go
@@ -0,0 +1,67 @@
+// @Author Ben.Zheng
+// @DateTime 2022/8/18 13:37
+
+package po
+
+type ResourceGroup struct {
+	NanoIdFullMode
+	Name string `gorm:"column:name;type:nvarchar(32);index:idx_img_grp_name;<-;"`
+}
+
+func (i ResourceGroup) TableName() string {
+	return "idk_res_group"
+}
+
+// ResourceCore
+// @field OssURL Display the resource local save path or OSS
+// save URL.
+// @field Extension For image, png, svg, jpeg and gif will be
+// included. For attachment, doc, execl, ppt, pdf, zip, 7z,
+// tar.gz and so on are acceptable.
+// @field Checksum Used to check idk-doc resource integrity.
+// Potential algo including SHA-256(64 characters), SHA-512
+// (128 characters), MD5 and so on.
+type ResourceCore struct {
+	NanoIdFullMode
+	OssURL    string `gorm:"column:url;type:varchar(256);<-;"`
+	Extension string `gorm:"column:ext;type:varchar(8);<-;"`
+	GroupId   string `gorm:"column:res_group_id;type:varchar(21);index;<-;"`
+	Size      int64  `gorm:"column:size;type:bigint;<-;"`
+	Checksum  string `gorm:"column:checksum;type:varchar(128);<-;"`
+}
+
+type ImageCore struct {
+	ResourceCore
+	Watermark string `gorm:"column:watermark;type:nvarchar(32);<-;"`
+}
+
+type Image[T ImageCore] struct {
+	Core  T              `gorm:"embedded;"`
+	Group *ResourceGroup `gorm:"foreignKey:GroupId;"`
+}
+
+func (i Image[T]) TableName() string {
+	return "idk_res_image"
+}
+
+func (i Image[T]) GetCore() T {
+	return i.Core
+}
+
+type AttachmentCore struct {
+	ResourceCore
+	Name string `gorm:"column:name;type:nvarchar(32);index;<-;"`
+}
+
+type Attachment[T AttachmentCore] struct {
+	Core  T              `gorm:"embedded;"`
+	Group *ResourceGroup `gorm:"foreignKey:GroupId;"`
+}
+
+func (a Attachment[T]) TableName() string {
+	return "idk_res_attachment"
+}
+
+func (a Attachment[T]) GetCore() T {
+	return a.Core
+}
---
+++ test/resource_test.go
@@ -0,0 +1,47 @@
+// @Author Ben.Zheng
+// @DateTime 2022/8/18 14:57
+
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+
+	"github.com/benz9527/idk-doc/internal/pkg/model/po"
+)
+
+func Test_init_res_in_db(t *testing.T) {
+	asserter := assert.New(t)
+	var opts []fx.Option
+
+	opts = append(opts, fx.Provide(genDevTestSQLiteDB))
+	opts = append(opts, fx.Invoke(func(dbClient *gorm.DB, lifecycle fx.Lifecycle) {
+		lifecycle.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+
+				asserter.NotNil(dbClient)
+				err := dbClient.AutoMigrate(
+					&po.ResourceGroup{},
+					&po.Image[po.ImageCore]{},
+					&po.Attachment[po.AttachmentCore]{},
+				)
+				asserter.Nil(err)
+
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				return nil
+			},
+		})
+	}))
+
+	app := fx.New(opts...)
+	defer app.Stop(context.Background())
+	if err := app.Start(context.Background()); err != nil {
+		t.Failed()
+	}
+}
