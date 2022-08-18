cc2d8dff04f4241e576d921381cdb154ef1cd1e0
Ben.Zheng <benzheng.6dev9527@outlook.com>
2022-08-18 15:52:13 +0800

    Added tag and tag map table definitions and tests with empty sql.

--- internal/pkg/model/po/tag.go
+++ internal/pkg/model/po/tag.go
@@ -3,6 +3,29 @@
 
 package po
 
+// Tag
+// @field Type Example "00-000" first 2 char present the idk-doc assets type,
+// the second 4 char present the sub source type.
 type Tag struct {
-	Label string
+	AutoIncIdFullMode
+	Label string `gorm:"column:label;type:nvarchar(32);index:idx_tag_label_type_mixed;<-;"`
+	Type  string `gorm:"column:type;type:char(6);index:idx_tag_label_type_mixed;<-;"`
+}
+
+func (t Tag) TableName() string {
+	return "idk_tag"
+}
+
+// TagMap
+// Without the foreign key definition directly.
+// Actually, idk will use the table of "idk_tag" with other assets tables'
+// primary key to compose the data.
+type TagMap struct {
+	BaseMetaNumericId
+	AssetsId string `gorm:"column:assets_id;type:varchar(21);index:idx_tag_map_mixed;<-;"`
+	TagId    uint   `gorm:"column:tag_id;type:int;index:idx_tag_map_mixed;<-;"`
+}
+
+func (t TagMap) TableName() string {
+	return "idk_tag_map"
 }
